32ea3e1ab98ccb6e93bf9999da370829779b9350
ehddnr301 <dy950328@gmail.com>
2021-09-14 17:39:07 +0900

    Test visual update

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -4,61 +4,75 @@ import { mlopsAPI } from "../../api";
 import HomePresenter from "./HomePresenter";
 
 const useInput = (defaultValue) => {
-    const [value, setValue] = useState(defaultValue)
+  const [value, setValue] = useState(defaultValue);
 
-    const onChange = (e) => {
-        const {
-            target: {value}
-        } = e;
-        setValue(value)
-    }
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setValue(value);
+  };
 
-    return {value, onChange, setValue}
-}
+  return { value, onChange, setValue };
+};
 
 const useSelect = (defaultValue) => {
-    const [value, setValue] = useState(defaultValue)
+  const [value, setValue] = useState(defaultValue);
 
-    const onChange = (e) => {
-        const {
-            target: {value}
-        } = e;
-        setValue(value)
-    }
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setValue(value);
+  };
 
-    return {value, onChange, setValue}
-}
+  return { value, onChange, setValue };
+};
 
 const HomeContainer = () => {
-    const [result, setResult] = useState(null);
-    const age = useInput(0);
-    const bmi = useInput(0);
-    const children = useInput(0);
-    const sex = useSelect('male');
-    const smoker = useSelect('no');
-    const region = useSelect('southeast');
+  const [result, setResult] = useState(null);
+  const [task, setTask] = useState(true);
+  const age = useInput(0);
+  const bmi = useInput(0);
+  const children = useInput(0);
+  const sex = useSelect("male");
+  const smoker = useSelect("no");
+  const region = useSelect("southeast");
 
-    const getData = useCallback( async() => {
-        const res = await axios.put('http://127.0.0.1:8000/predict/insurance?model_name=keep_update_model', {
-            age:0,
-            sex:0,
-            bmi:0.0,
-            children:0,
-            smoker:0,
-            region:0
-        })
-        console.log(res)
-    })
-    
-    return <HomePresenter 
-            getData={getData}
-            age= {age} 
-            bmi= {bmi} 
-            children= {children} 
-            sex= {sex} 
-            smoker= {smoker} 
-            region= {region} 
-            />
+  const getData = useCallback(async () => {
+    const res = await axios.put(
+      "http://127.0.0.1:8000/predict/insurance?model_name=keep_update_model",
+      {
+        age: 0,
+        sex: 0,
+        bmi: 0.0,
+        children: 0,
+        smoker: 0,
+        region: 0,
+      }
+    );
+    console.log(res);
+    setResult(res);
+  });
+
+  const toggleTask = useCallback(() => {
+    setTask((task) => !task);
+  });
+
+  return (
+    <HomePresenter
+      getData={getData}
+      age={age}
+      bmi={bmi}
+      children={children}
+      sex={sex}
+      smoker={smoker}
+      region={region}
+      result={result}
+      task={task}
+      toggleTask={toggleTask}
+    />
+  );
 };
 
 export default HomeContainer;
--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,44 +1,147 @@
 import React from "react";
 import styled from "styled-components";
-import { mlopsAPI } from "../../api";
 
-const InsuranceContainer = styled.div`
+const changeTemp = (r, t) => {
+  const result = document.getElementById(r);
+  const temp = document.getElementById(t);
 
+  temp.innerText = result.value;
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
-const Wrapper = styled.div``
+const H2 = styled.h2`
+  font-size: 24px;
+`;
 
-const Input = styled.input``
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
-const Select = styled.select``
+const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 20px 0;
+`;
 
-const HomePresenter = ({getData,age,bmi,children,sex,smoker,region}) => {
-    
-    return (
-    <InsuranceContainer>
-        <Wrapper>
-            <Input placeholder={'Age'} {...age}/>
-            <Input placeholder={'Bmi'} {...bmi}/>
-            <Input placeholder={'Children'} {...children}/>
-            <Select {...sex}>
-                <option value='male'>Male</option>
-                <option value='female'>Female</option>
-            </Select>
-            <Select {...smoker}>
-                <option value='no'>No</option>
-                <option value='yes'>Yes</option>
-            </Select>
-            <Select {...region}>
-                <option value='southeast'>Southeast</option>
-                <option value='southwest'>Southwest</option>
-                <option value='northwest'>Northwest</option>
-                <option value='northeast'>Northeast</option>
-            </Select>
-            <button onClick={getData}>
-                    예측해줘!</button>
-        </Wrapper>
-    </InsuranceContainer>    
-    );
-}
+const H3 = styled.h3`
+  font-size: 16px;
+  margin-bottom: 5px;
+`;
 
-export default HomePresenter
+const Input = styled.input``;
+
+const Select = styled.select``;
+
+const Btn = styled.button`
+  border: 1px solid white;
+  padding: 10px 5px;
+  background-color: #fff;
+  border-radius: 15px;
+  transition: 0.3s;
+  cursor: pointer;
+  &:hover {
+    color: white;
+    background-color: #23b499;
+    border: 1px solid black;
+  }
+`;
+const Result = styled.div``;
+
+const Temp = styled.div`
+  margin-bottom: 5px;
+`;
+
+const HomePresenter = ({
+  getData,
+  age,
+  bmi,
+  children,
+  sex,
+  smoker,
+  region,
+  result,
+  task,
+  toggleTask,
+}) => {
+  return task ? (
+    <Container>
+      <H2 onClick={toggleTask}>Predict Insurance</H2>
+      <Wrapper>
+        <Box>
+          <H3>Age를 입력해주세요.</H3>
+          <Input placeholder={"Age"} {...age} />
+        </Box>
+        <Box>
+          <H3>BMI를 입력해주세요.</H3>
+          <Input placeholder={"BMI"} {...bmi} />
+        </Box>
+        <Box>
+          <H3>Children 수를 입력해주세요.</H3>
+          <Input placeholder={"Children"} {...children} />
+        </Box>
+        <Box>
+          <H3>성별을 선택해주세요.</H3>
+          <Select {...sex}>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </Select>
+        </Box>
+        <Box>
+          <H3>흡연여부를 선택해주세요.</H3>
+          <Select {...smoker}>
+            <option value="no">No</option>
+            <option value="yes">Yes</option>
+          </Select>
+        </Box>
+        <Box>
+          <H3>거주지역을 선택해주세요.</H3>
+          <Select {...region}>
+            <option value="southeast">Southeast</option>
+            <option value="southwest">Southwest</option>
+            <option value="northwest">Northwest</option>
+            <option value="northeast">Northeast</option>
+          </Select>
+        </Box>
+        <Btn onClick={getData}>Make Prediction</Btn>
+      </Wrapper>
+      {result ? <Result>1</Result> : <Result>2</Result>}
+    </Container>
+  ) : (
+    <Container>
+      <H2 onClick={toggleTask}>Predict Temperature</H2>
+      <Wrapper>
+        {[1, 2, 3, 4, 5, 6, 7].map((i) => {
+          return (
+            <Box>
+              <Temp>
+                {i !== 7
+                  ? `${i * 10}번째까지 온도 평균을 설정해주세요`
+                  : `72번째까지 온도 평균을 설정해주세요`}
+              </Temp>
+              <Input
+                id={`r${i}`}
+                type="range"
+                min={-10}
+                max={40}
+                step={0.1}
+                onChange={() => changeTemp(`r${i}`, `t${i}`)}
+              ></Input>
+              <div id={`t${i}`}></div>
+            </Box>
+          );
+        })}
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default HomePresenter;
